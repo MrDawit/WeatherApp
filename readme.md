e8949bed78f0d2b2fce3df69b9d849b5038f7096
shalom dawit <shalom.dawit@gmail.com>
2020-10-06 01:01:01 -0400

    functioning search history, some html/css stylizing, connection with api and updated readme

--- readme.md
+++ readme.md
@@ -1,7 +1,39 @@
 ## The Ultimate...simple WeatherApp
+A weather app that gives the user their searched city's current and future forecast. 
+
+live link: https://MrDawit.github.io/WeatherApp 
 
 ## Description
+After searching for a city, this WeatherApp uses a weather api to make important weather information available for the user. Weather information that includes weather conditions, the temperature, the humidity, the wind speed and the UV index.
 
 ## UserStory
+```
+AS A traveler
+I WANT to see the weather outlook for multiple cities
+SO THAT I can plan a trip accordingly
+```
+## Technologies Used:
+* Javascript
+* Jquery library
+* Moment library
+* AJAX
+* DOM
+* localStorage API
+* HTML
+* CSS
 
-## Edits
+## Files Included:
+* script.js
+* moment.js
+* index.html
+* style.css
+* readme.md
+
+## Notes
+* Weather api information from www.openweathermap.org
+
+## Created By:
+Shalom Dawit\
+LinkedIn:https://www.linkedin.com/in/shalom-dawit-a0a5a4126 \
+GitHub:https://github.com/MrDawit \
+Email:Shalom.Dawit@gmail.com \
--- script.js
+++ script.js
@@ -1,7 +1,103 @@
-var queryURL="http://api.openweathermap.org/data/2.5/weather?q=hartford,connecticut&appid=715ee435d9e6cc809bc1cb6b62581405"
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-$("#currentForecast").append(response);
-  });
+$(document).ready(function(){
+
+    var city=null;
+    var queryURL="http://api.openweathermap.org/data/2.5/weather?q=hartford,connecticut&appid=715ee435d9e6cc809bc1cb6b62581405"
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+    $("#cityName").append(response.name);
+      });
+    
+    
+        /* sidenav opener */
+        function openNav() {
+          document.getElementById("searchSidenav").style.width = "400px";
+          document.getElementById("main").style.marginLeft = "400px";
+        }
+    
+        /* sidenav closer */
+        function closeNav() {
+          document.getElementById("searchSidenav").style.width = "0";
+          document.getElementById("main").style.marginLeft = "0";
+        }
+    
+        $("button").click(function () {
+          openNav();
+        
+          for (i = 1; i <= localStorage.length; i++) {
+            $("#history").append(localStorage[i] + "<br>");
+          };
+        });
+    
+    
+        $(".closebtn").click(function () {
+          closeNav()
+          $("#history").clear();
+        });
+    
+        $("form").submit(function (event) {
+          event.preventDefault();
+          //closeNav()
+          var city = $("input").val();
+    
+         
+    
+    
+          //var prevCities=[];
+          //if(localStorage!==null || undefined){
+          //localStorage.setItem("preCities",city);
+          //var cities=JSON.stringify(localStorage.getItem("preCities"));
+          //prevCities.push=cities;
+          localStorage.setItem(localStorage.length + 1, city);
+    
+          $("#history").append(localStorage[localStorage.length] + "<br>");
+          //}
+          //var i={a:{"city":city}}
+          //var city = document.getElementsByTagName("input")[0].value
+    
+          
+        });
+    
+      
+    
+        
+    
+    
+    //localStorage for objects refresher example:
+    //start
+    // var food={meal:"steak"};
+    // var food_serialized=JSON.stringify(food);
+    // //console.log(food_serialized);
+    // localStorage.setItem("foodie",food_serialized);
+    // //console.log(localStorage);
+    // var pToTheArsed=JSON.parse(localStorage.getItem("foodie"));
+    // console.log(pToTheArsed); 
+    //end
+    
+    
+    //$("#currentForecast").append("<div></div>");
+    //const DIV=$("div").attr("id","city");
+    
+    
+    
+    
+    
+    
+    // var queryURL="http://api.openweathermap.org/data/2.5/weather?q=hartford,connecticut&appid=715ee435d9e6cc809bc1cb6b62581405"
+    
+    // $.ajax({
+    //     url: queryURL,
+    //     method: "GET"
+    //   }).then(function(response) {
+    //   //  const CITY=$("#city").html(response.name);
+    
+    //     //var currentForecVar=$("#currentForecast");
+    //     const DIV=$("<div>");
+    // var inDiv =$("<div>").text("City : "+ response.name);
+    //   DIV.append(inDiv);
+    // $("#currentForecast").append(DIV);
+    // console.log(response.name);
+    //   });
+    
+     });

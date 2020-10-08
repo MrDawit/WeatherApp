cf68b7764f3fe8a1dc7886cbf3064d268559b12c
shalom dawit <shalom.dawit@gmail.com>
2020-10-08 00:34:01 -0400

    search history is now working link to api call. version 1 of button designs

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
   var city = null;
   var lat = null;
   var lon = null;
+  var searchedCity = null;
 
 
   /* sidenav opener */
@@ -47,21 +48,62 @@ $(document).ready(function () {
 
 
 
-
+//event listener to open sidenav
   $("button").click(function () {
     openNav();
-
+//creating list of searched cities
     for (i = 1; i <= localStorage.length; i++) {
-      $("#history").append(localStorage[i] + "<br>");
+      $("#searchSidenav").append("<button class='glow-on-hover historyBTN'>"+localStorage[i]+"</button><br>" );
     };
+//changed li to button to test
+  //event listener to have a new api call using previously searched city
+  $(".historyBTN").click(function(){
+    searchedCity=$(this).html();
+    $("#cityName").empty();
+        $(".card-body").empty();
+        
+    
+    // (searched city) current weather response with uv index response nested inside
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchedCity + "&units=imperial&appid=715ee435d9e6cc809bc1cb6b62581405";
+       
+       $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function (response) {
+        $("#cityName").append(response.name + "<img src='https://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png'> <br>");
+        $(".card-body").append("<span> Time: </span>" + response.dt + " UTC (Unix time)<br>");
+        $(".card-body").append("<span> Temperature: </span>" + response.main.temp + "&#8457<br>");
+        $(".card-body").append("<span> Temp feels like: </span>" + response.main.feels_like + "&#8457<br>");
+        $(".card-body").append("<span> Humidity: </span>" + response.main.humidity + "&#37;<br>");
+        $(".card-body").append("<span> Weather Condition: </span>" + response.weather[0].main + ", " + response.weather[0].description + "<br>");
+        $(".card-body").append("<span> Windspead & Direction: </span>" + response.wind.speed + " mph" + " at " + response.wind.deg + "&deg;<br>");
+    
+        //using the current weather response to get lat and lon, inorder to get UV index response
+        var queryURL2 = "https://api.openweathermap.org/data/2.5/uvi?lat=" + response.coord.lat + "&lon=" + response.coord.lon + "&appid=715ee435d9e6cc809bc1cb6b62581405";
+    
+        $.ajax({
+          url: queryURL2,
+          method: "GET"
+        }).then(function (response) {
+          $(".card-body").append("<span> UV Index: </span>" + response.value + "<br>");
+        });
+      });
+    
+    
+    });
+
+
+
   });
 
 
+
+//event listener to close sidenav
   $(".closebtn").click(function () {
     closeNav()
     $("#history").empty();
   });
-
+//event listener when city is entered
   $("form").submit(function (event) {
 
     event.preventDefault();

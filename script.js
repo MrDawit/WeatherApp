8212f65592352c009a626de0c88814fa7958c5b2
shalom dawit <shalom.dawit@gmail.com>
2020-10-08 01:55:33 -0400

    uv index color code functionality added

--- script.js
+++ script.js
@@ -17,6 +17,35 @@ $(document).ready(function () {
     document.getElementById("searchSidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
   }
+function currentCityFunction(){
+     // (searched city) current weather response with uv index response nested inside
+     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchedCity + "&units=imperial&appid=715ee435d9e6cc809bc1cb6b62581405";
+       
+     $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function (response) {
+      $("#cityName").append(response.name + "<img src='https://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png'> <br>");
+      $(".card-body").append("<span> Time: </span>" + response.dt + " UTC (Unix time)<br>");
+      $(".card-body").append("<span> Temperature: </span>" + response.main.temp + "&#8457<br>");
+      $(".card-body").append("<span> Temp feels like: </span>" + response.main.feels_like + "&#8457<br>");
+      $(".card-body").append("<span> Humidity: </span>" + response.main.humidity + "&#37;<br>");
+      $(".card-body").append("<span> Weather Condition: </span>" + response.weather[0].main + ", " + response.weather[0].description + "<br>");
+      $(".card-body").append("<span> Windspead & Direction: </span>" + response.wind.speed + " mph" + " at " + response.wind.deg + "&deg;<br>");
+  
+      //using the current weather response to get lat and lon, inorder to get UV index response
+      var queryURL2 = "https://api.openweathermap.org/data/2.5/uvi?lat=" + response.coord.lat + "&lon=" + response.coord.lon + "&appid=715ee435d9e6cc809bc1cb6b62581405";
+  
+      $.ajax({
+        url: queryURL2,
+        method: "GET"
+      }).then(function (response) {
+        $(".card-body").append("<span> UV Index: </span> <div class='uvIndex'>" + response.value + "</div> <br>");
+      });
+    });
+  
+}
+  
   // (predetermined city) current weather response with uv index response nested inside
   $.ajax({
     url: "https://api.openweathermap.org/data/2.5/weather?q=hartford,connecticut&units=imperial&appid=715ee435d9e6cc809bc1cb6b62581405",
@@ -37,7 +66,20 @@ $(document).ready(function () {
       url: queryURL2,
       method: "GET"
     }).then(function (response) {
-      $(".card-body").append("<span> UV Index: </span>" + response.value + "<br>");
+      $(".card-body").append("<span> UV Index: </span> <span class='uvIndex'>" + response.value + "</span> <br>");
+      if(response.value<3){
+        $(".uvIndex").css("background-color","green");
+      }
+      if(response.value>=3&&response.value<6){
+        $(".uvIndex").css("background-color","yellow");
+      }
+      if(response.value>=6){
+        $(".uvIndex").style("background-color","red");
+      }
+      //when 'else' statement included, undefined is presented when it should not. maybe this has to do with 2nd 'if' statement
+      // else{
+      //   $(".uvIndex").text("undefined");
+      // };
     });
 
 
@@ -61,34 +103,9 @@ $(document).ready(function () {
     searchedCity=$(this).html();
     $("#cityName").empty();
         $(".card-body").empty();
-        
+        currentCityFunction();
     
-    // (searched city) current weather response with uv index response nested inside
-        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchedCity + "&units=imperial&appid=715ee435d9e6cc809bc1cb6b62581405";
-       
-       $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).then(function (response) {
-        $("#cityName").append(response.name + "<img src='https://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png'> <br>");
-        $(".card-body").append("<span> Time: </span>" + response.dt + " UTC (Unix time)<br>");
-        $(".card-body").append("<span> Temperature: </span>" + response.main.temp + "&#8457<br>");
-        $(".card-body").append("<span> Temp feels like: </span>" + response.main.feels_like + "&#8457<br>");
-        $(".card-body").append("<span> Humidity: </span>" + response.main.humidity + "&#37;<br>");
-        $(".card-body").append("<span> Weather Condition: </span>" + response.weather[0].main + ", " + response.weather[0].description + "<br>");
-        $(".card-body").append("<span> Windspead & Direction: </span>" + response.wind.speed + " mph" + " at " + response.wind.deg + "&deg;<br>");
-    
-        //using the current weather response to get lat and lon, inorder to get UV index response
-        var queryURL2 = "https://api.openweathermap.org/data/2.5/uvi?lat=" + response.coord.lat + "&lon=" + response.coord.lon + "&appid=715ee435d9e6cc809bc1cb6b62581405";
-    
-        $.ajax({
-          url: queryURL2,
-          method: "GET"
-        }).then(function (response) {
-          $(".card-body").append("<span> UV Index: </span>" + response.value + "<br>");
-        });
-      });
-    
+ 
     
     });
 
@@ -136,19 +153,13 @@ $(document).ready(function () {
       $(".card-body").append("<span> UV Index: </span>" + response.value + "<br>");
     });
   });
-    //var prevCities=[];
-    //if(localStorage!==null || undefined){
-    //localStorage.setItem("preCities",city);
-    //var cities=JSON.stringify(localStorage.getItem("preCities"));
-    //prevCities.push=cities;
+  
     localStorage.setItem(localStorage.length + 1, city);
 
     $("#history").append(localStorage[localStorage.length] + "<br>");
 
     $("input").val("");
-    //}
-    //var i={a:{"city":city}}
-    //var city = document.getElementsByTagName("input")[0].value
+  
 
 
   });
@@ -156,42 +167,5 @@ $(document).ready(function () {
 
 
 
-
-
-  //localStorage for objects refresher example:
-  //start
-  // var food={meal:"steak"};
-  // var food_serialized=JSON.stringify(food);
-  // //console.log(food_serialized);
-  // localStorage.setItem("foodie",food_serialized);
-  // //console.log(localStorage);
-  // var pToTheArsed=JSON.parse(localStorage.getItem("foodie"));
-  // console.log(pToTheArsed); 
-  //end
-
-
-  //$("#currentForecast").append("<div></div>");
-  //const DIV=$("div").attr("id","city");
-
-
-
-
-
-
-  // var queryURL="http://api.openweathermap.org/data/2.5/weather?q=hartford,connecticut&appid=715ee435d9e6cc809bc1cb6b62581405"
-
-  // $.ajax({
-  //     url: queryURL,
-  //     method: "GET"
-  //   }).then(function(response) {
-  //   //  const CITY=$("#city").html(response.name);
-
-  //     //var currentForecVar=$("#currentForecast");
-  //     const DIV=$("<div>");
-  // var inDiv =$("<div>").text("City : "+ response.name);
-  //   DIV.append(inDiv);
-  // $("#currentForecast").append(DIV);
-  // console.log(response.name);
-  //   });
 
 });
